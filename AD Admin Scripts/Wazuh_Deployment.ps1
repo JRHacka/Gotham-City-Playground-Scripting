82fad693b05db4367024b75a5526de9dc3171b5b
Jake Rice <92770711+JRHacka@users.noreply.github.com>
2023-08-29 21:34:54 -0700

    Updating things
    Updated the main welcome page, as well as how to use that powershell command.

--- AD Admin Scripts/Wazuh_Deployment.ps1
+++ AD Admin Scripts/Wazuh_Deployment.ps1
@@ -1,3 +1,4 @@
 ## Wazuh Agent Deployment
+## Want to work on fully automating this, but now it is as ismple as running this command in powershell as admin.
 
 Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.5.1-1.msi -OutFile ${env:tmp}\wazuh-agent.msi; msiexec.exe /i ${env:tmp}\wazuh-agent.msi /q WAZUH_MANAGER='192.168.10.140' WAZUH_REGISTRATION_SERVER='192.168.10.140' WAZUH_AGENT_GROUP='default'
--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
  ##Project 1 ----------- Completed
  Creating a script to bulk upload users from a csv with a randomly generated password.
+
+ ##Project 2 (Phase 1) ------------ Complete
+ Set up Wazuh and deployed in on all three devices I had previously set up. Now time to play with rules and whatnot, and investigate automating the deployment. 
  
