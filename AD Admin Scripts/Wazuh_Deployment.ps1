7c4c9c9f1681e81bad60c53120c93ac5b8f1e631
Jake Rice <92770711+JRHacka@users.noreply.github.com>
2024-01-29 19:37:27 -0600

    Update Wazuh_Deployment.ps1

--- AD Admin Scripts/Wazuh_Deployment.ps1
+++ AD Admin Scripts/Wazuh_Deployment.ps1
@@ -1,4 +1,8 @@
 ## Wazuh Agent Deployment
 ## Want to work on fully automating this, but now it is as ismple as running this command in powershell as admin.
 
-Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.5.1-1.msi -OutFile ${env:tmp}\wazuh-agent.msi; msiexec.exe /i ${env:tmp}\wazuh-agent.msi /q WAZUH_MANAGER='192.168.10.140' WAZUH_REGISTRATION_SERVER='192.168.10.140' WAZUH_AGENT_GROUP='default'
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.5.1-1.msi -OutFile ${env:tmp}\wazuh-agent.msi; msiexec.exe /i ${env:tmp}\wazuh-agent.msi/q WAZUH_MANAGER='192.168.10.140' WAZUH_REGISTRATION_SERVER='192.168.10.140' WAZUH_AGENT_GROUP='default'
+
+
+##Updated Version
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.7.2-1.msi -OutFile ${env.tmp}\wazuh-agent; msiexec.exe /i ${env.tmp}\wazuh-agent /q WAZUH_MANAGER='192.168.172.10' WAZUH_AGENT_GROUP='default' WAZUH_REGISTRATION_SERVER='192.168.172.10' 
