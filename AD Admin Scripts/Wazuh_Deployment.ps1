a2a834daa6ee4321245ffc28c1f0710a02c5071e
Jake Rice <92770711+JRHacka@users.noreply.github.com>
2024-01-29 19:38:18 -0600

    Update Wazuh_Deployment.ps1

--- AD Admin Scripts/Wazuh_Deployment.ps1
+++ AD Admin Scripts/Wazuh_Deployment.ps1
@@ -4,5 +4,5 @@
 Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.5.1-1.msi -OutFile ${env:tmp}\wazuh-agent.msi; msiexec.exe /i ${env:tmp}\wazuh-agent.msi/q WAZUH_MANAGER='192.168.10.140' WAZUH_REGISTRATION_SERVER='192.168.10.140' WAZUH_AGENT_GROUP='default'
 
 
-##Updated Version
+##Updated Version 
 Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.7.2-1.msi -OutFile ${env.tmp}\wazuh-agent; msiexec.exe /i ${env.tmp}\wazuh-agent /q WAZUH_MANAGER='192.168.172.10' WAZUH_AGENT_GROUP='default' WAZUH_REGISTRATION_SERVER='192.168.172.10' 
